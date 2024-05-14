938ce82a305db7995c8c441744055b7df30c203e
mastereng12 <viktor.miller.eng@gmail.com>
2024-05-14 18:52:04 -0400

    password path

--- utils/utils.go
+++ utils/utils.go
@@ -23,31 +23,55 @@ func ConvertBLSKey(blsPubKey []byte) ([2]*big.Int, error) {
 }
 
 // DisplayWarningMessage displays a warning message with ASCII art.
-func DisplayWarningMessage(keyType, privateKey, ksPath string) {
+func DisplayWarningMessage(keyType, privateKey, ksPath string) error {
 	// Clear the screen
-	cmd := exec.Command("clear") // For Windows use "cls"
+	cmd := exec.Command("less", "-R") // For Windows use "cls"
 	cmd.Stdout = os.Stdout
-	if err := cmd.Run(); err != nil {
-		fmt.Println("failed to clear the screen: ", err)
+	cmd.Stderr = os.Stderr
+
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		return fmt.Errorf("failed to create stdin pipe: %w", err)
+	}
+	if err := cmd.Start(); err != nil {
+		return fmt.Errorf("failed to start command: %w", err)
 	}
 
+	msg := ""
 	// Display warning message with ASCII art and colored text
-	fmt.Println("\033[1m\x1b[36m#########################################################################\033[0m")
-	fmt.Println("\033[1m\x1b[36m#  _        _____    _____   _____    _____   _     _   _____   ______  #\033[0m")
-	fmt.Println("\033[1m\x1b[36m# (_)      (_____)  (_____) (_____)  (_____) (_)   (_) (_____) (______) #\033[0m")
-	fmt.Println("\033[1m\x1b[36m# (_)     (_)___(_)(_)  ___ (_)__(_)(_)___(_)(__)_ (_)(_)  ___ (_)__    #\033[0m")
-	fmt.Println("\033[1m\x1b[36m# (_)     (_______)(_) (___)(_____) (_______)(_)(_)(_)(_) (___)(____)   #\033[0m")
-	fmt.Println("\033[1m\x1b[36m# (_)____ (_)   (_)(_)___(_)( ) ( ) (_)   (_)(_)  (__)(_)___(_)(_)____  #\033[0m")
-	fmt.Println("\033[1m\x1b[36m# (______)(_)   (_) (_____) (_)  (_)(_)   (_)(_)   (_) (_____) (______) #\033[0m")
-	fmt.Println("\033[1m\x1b[36m#########################################################################\033[0m")
+	msg += fmt.Sprintln("\033[1m\x1b[36m#########################################################################\033[0m")
+	msg += fmt.Sprintln("\033[1m\x1b[36m#  _        _____    _____   _____    _____   _     _   _____   ______  #\033[0m")
+	msg += fmt.Sprintln("\033[1m\x1b[36m# (_)      (_____)  (_____) (_____)  (_____) (_)   (_) (_____) (______) #\033[0m")
+	msg += fmt.Sprintln("\033[1m\x1b[36m# (_)     (_)___(_)(_)  ___ (_)__(_)(_)___(_)(__)_ (_)(_)  ___ (_)__    #\033[0m")
+	msg += fmt.Sprintln("\033[1m\x1b[36m# (_)     (_______)(_) (___)(_____) (_______)(_)(_)(_)(_) (___)(____)   #\033[0m")
+	msg += fmt.Sprintln("\033[1m\x1b[36m# (_)____ (_)   (_)(_)___(_)( ) ( ) (_)   (_)(_)  (__)(_)___(_)(_)____  #\033[0m")
+	msg += fmt.Sprintln("\033[1m\x1b[36m# (______)(_)   (_) (_____) (_)  (_)(_)   (_)(_)   (_) (_____) (______) #\033[0m")
+	msg += fmt.Sprintln("\033[1m\x1b[36m#########################################################################\033[0m")
 
-	fmt.Println("")
-	fmt.Println(strings.ToUpper(keyType) + " Private Key Generated Successfully 🎉")
+	msg += fmt.Sprintln("")
+	msg += fmt.Sprintln(strings.ToUpper(keyType) + " Private Key Generated Successfully 🎉")
 	border := strings.Repeat("=", len(privateKey)+6)
-	fmt.Println("\033[1m\x1b[31m" + border + "\033[0m")
-	fmt.Println("\x1b[36m|  " + privateKey + "  |\033[0m")
-	fmt.Println("\033[1m\x1b[31m" + border + "\033[0m")
-	fmt.Println("")
-	fmt.Println("\033[1m\x1b[33m🔑  WARNING: Make sure to copy this private key securely and never share it with anyone!\033[0m")
-	fmt.Println("🔑  Keystore file saved at: " + ksPath)
+	msg += fmt.Sprintln("\033[1m\x1b[31m" + border + "\033[0m")
+	msg += fmt.Sprintln("\x1b[36m|  " + privateKey + "  |\033[0m")
+	msg += fmt.Sprintln("\033[1m\x1b[31m" + border + "\033[0m")
+	msg += fmt.Sprintln("")
+	msg += fmt.Sprintln("\033[1m\x1b[33m🔑  WARNING: Make sure to copy this private key securely and never share it with anyone!\033[0m")
+	msg += fmt.Sprintln("🔑  Keystore file saved at: " + ksPath)
+
+	// Write the message to the command
+	if _, err = stdin.Write([]byte(msg)); err != nil {
+		return fmt.Errorf("failed to write to stdin: %w", err)
+	}
+
+	// Close the stdin pipe
+	if err := stdin.Close(); err != nil {
+		return fmt.Errorf("failed to close stdin: %w", err)
+	}
+
+	// Wait for the command to finish
+	if err := cmd.Wait(); err != nil {
+		return fmt.Errorf("failed to wait for command: %w", err)
+	}
+
+	return nil
 }
