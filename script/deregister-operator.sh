d907fd8213e357e94fdef79c31e88b6a82cede75
ChainDev931105 <ethanackerland@gmail.com>
2024-05-24 07:17:50 -0500

    remove committee

--- script/deregister-operator.sh
+++ script/deregister-operator.sh
@@ -9,7 +9,7 @@
 # ==== MODIFY ME! ====
 ETH_KEY=
 OPERATOR_ADDR=
-export ETH_RPC_URL=https://rpc.holesky.ethpandaops.io
+export ETH_RPC_URL= # Put rpc url here
 
 # --- ChainIDS ---
 HOLESKY_CHAINID=17000
@@ -33,21 +33,17 @@ set_contract_addresses() {
     if [ "$chain_id" -eq "$HOLESKY_CHAINID" ]; then
         echo "Registering Operator on Holesky Testnet"
         LAGRANGE_SERVICE_ADDR=0x18A74E66cc90F0B1744Da27E72Df338cEa0A542b
-        LAGRANGE_COMMITTEE_ADDR=0x57BAf26C77BBBa3D3A8Bd620c8d74B44Bfda8818
         EIGEN_AVS_DIRECTORY_ADDR=0x055733000064333CaDDbC92763c58BF0192fFeBf
     elif [ "$chain_id" -eq "$MAINNET_CHAINID" ]; then
         LAGRANGE_SERVICE_ADDR=0x35F4f28A8d3Ff20EEd10e087e8F96Ea2641E6AA2
-        LAGRANGE_COMMITTEE_ADDR=0xECc22f3EcD0EFC8aD77A78ad9469eFbc44E746F5
         EIGEN_AVS_DIRECTORY_ADDR=0x135DDa560e946695d6f155dACaFC6f1F25C1F5AF
     elif [ "$chain_id" -eq "$SEPOLIA_CHAINID" ]; then
         echo "Registering Operator on Sepolia Testnet"
         LAGRANGE_SERVICE_ADDR=0x34d8f7384Ddd4e8Ab447a150F9955f284882A43F
-        LAGRANGE_COMMITTEE_ADDR=0x298baE5B68bA10e0dF7d98C5b19Aba7784d74F71
         EIGEN_AVS_DIRECTORY_ADDR=0xE5F13E61654363719407247BB7E06f539d3a9d32
     elif [ "$chain_id" -eq "$LOCAL_CHAINID" ]; then
         echo "Registering Operator on Sepolia Testnet"
         LAGRANGE_SERVICE_ADDR=0xBda41273d671bb33374F7b9C4Ae8746c712727f7
-        LAGRANGE_COMMITTEE_ADDR=0xBF4E09354df24900e3d2A1e9057a9F7601fbDD06
         EIGEN_AVS_DIRECTORY_ADDR=0x6Bf0fF4eBa00E3668c0241bb1C622CDBFE55bbE0
     else
         echo "Unknown chain ID: $chain_id"
